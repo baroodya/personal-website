5eeea231ce4756441c84b7e0440c6f93c99fcc4a
Alex Baroody <abaroody@princeton.edu>
2021-12-29 23:33:38 -0500

    Get index header scrolling working

---
+++ public/javascripts/index.js
@@ -0,0 +1,19 @@
+
+let windowHeightPixels = window.innerHeight;
+let startingHeaderHeight = windowHeightPixels / 2;
+console.log(windowHeightPixels);
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+   let scrollHeight = document.documentElement.scrollTop;
+   let headerHeight = startingHeaderHeight - scrollHeight;
+
+   if (headerHeight > (windowHeightPixels / 10)) {
+      // console.log(String(startingHeaderHeight - scrollHeight) + "px");
+      document.getElementById("header").style.height = String(headerHeight) + "px";
+   } else {
+      document.getElementById("header").style.height = "10%";
+   }
+}
